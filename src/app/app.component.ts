eed4501d4f5abb20421792c5e62f80004ca2d646
shutter0818 <ashutm18802@gmail.com>
2025-06-13 10:18:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'templateForms';
+  name:string="";
+  email:string="";
+  country:string="";
+  city:string="";
+
+  countries=[
+    "usa","uk","india"
+  ]
+
+  cities: {[key:string]:string[]}={
+    usa:['New York','LA','Chicago'],
+    uk:['London','Dublin','Glasgow'],
+    india:['Delhi','Mumbai','Chennai','Kolkata']
+  }
+
+  submitForm(myForm:NgForm){
+    if(myForm.valid){
+      const formValues=JSON.stringify(myForm.value);
+      console.log(formValues)
+      alert("Form submitted successfully!")
+    }
+    else{
+      alert("Please fill up the data")
+    }
+  }
+
+  resetValue(myForm:NgForm){
+    myForm.resetForm();
+  }
+  
+  getCountriesById(country:string){
+    return this.cities[country] || []
+  }
 }
